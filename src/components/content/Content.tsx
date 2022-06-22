df0d65e4f1ca9bc98414930bfd7e5c82b9fef76f
lauren olivier <laurenzolivier@gmail.com>
2022-06-22 14:36:03 +0200

    feat: add footer component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,8 +1,9 @@
 import dynamic from 'next/dynamic'
-import { useTheme } from '@okp4/ui'
-import type { ThemeContextType } from '@okp4/ui'
+import { Footer, loadTranslations, Typography, useTheme, useTranslation } from '@okp4/ui'
+import type { ThemeContextType, UseTranslationResponse } from '@okp4/ui'
 import lightCosmos from '@okp4/ui/lib/assets/images/cosmos-clear.png'
 import darkCosmos from '@okp4/ui/lib/assets/images/cosmos-dark.png'
+import { translationsToLoad } from '../../i18n/index'
 
 const HeaderWithoutSSR = dynamic(async () => import('../header/Header'), {
   ssr: false
@@ -12,16 +13,49 @@ const FaucetWithoutSSR = dynamic(async () => import('../faucet/Faucet'), {
   ssr: false
 })
 
-const ContentWrapper: React.FC = () => {
+const languages = [
+  {
+    name: 'English',
+    lng: 'en'
+  },
+  {
+    name: 'Français',
+    lng: 'fr'
+  }
+]
+
+const Okp4Link = (): JSX.Element => {
+  const { t }: UseTranslationResponse = useTranslation()
+
+  return (
+    <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="xlight" noWrap>
+      {`${t('footer:brand-link')} `}
+      <Typography color="highlighted-text" fontSize="x-small" fontWeight="bold">
+        <a
+          href="https://okp4.network/"
+          rel="author noreferrer"
+          style={{ textDecoration: 'unset', color: 'inherit' }}
+          target="_blank"
+        >
+          ØKP4
+        </a>
+      </Typography>
+    </Typography>
+  )
+}
+
+const Content: React.FC = () => {
   const { theme }: ThemeContextType = useTheme()
   const themedImage = theme === 'light' ? lightCosmos.src : darkCosmos.src
+  loadTranslations(translationsToLoad)
 
   return (
     <div className="okp4-faucet-testnet-content" style={{ backgroundImage: `url(${themedImage})` }}>
       <HeaderWithoutSSR />
       <FaucetWithoutSSR />
+      <Footer languages={languages} lastElement={<Okp4Link />} />
     </div>
   )
 }
 
-export default ContentWrapper
+export default Content
---
+++ src/i18n/index.ts
@@ -0,0 +1,7 @@
+import footer_en from './footer_en.json'
+import footer_fr from './footer_fr.json'
+
+export const translationsToLoad = [
+    { lng: 'en', namespace: 'footer', resource: footer_en },
+    { lng: 'fr', namespace: 'footer', resource: footer_fr }
+  ]
