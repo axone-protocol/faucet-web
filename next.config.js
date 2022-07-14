09c29d5542cbc0d17e27e485813e4a1bea973392
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-07-14 09:59:02 +0200

    feat: allow set config at runtime

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    CHAIN_ID: process.env.CHAIN_ID,
-    FAUCET_URL: process.env.FAUCET_URL,
-    CHAIN_NAME: process.env.CHAIN_NAME,
-    CHAIN_RPC_ENDPOINT: process.env.CHAIN_RPC_ENDPOINT,
-    CHAIN_REST_ENDPOINT: process.env.CHAIN_REST_ENDPOINT
+  serverRuntimeConfig: {
+    CHAIN_ID: "okp4-devnet-1",
+    CHAIN_NAME: "OKP4",
+    CHAIN_RPC_ENDPOINT: "https://api.devnet.staging.okp4.network:443/rpc",
+    CHAIN_REST_ENDPOINT: "https://api.devnet.staging.okp4.network",
+    FAUCET_URL: "https://faucet.devnet.staging.okp4.network/graphql",
   },
-  experimental: {
-    outputStandalone: true
-  }
+  output: 'standalone',
 }
 
 module.exports = nextConfig
