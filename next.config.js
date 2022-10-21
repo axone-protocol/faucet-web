cc0cffe72326a5182fdbf5a8f767264340bce8f9
ccamel <camel.christophe@gmail.com>
2022-10-21 15:08:56 +0200

    chore: remove serverRuntimeConfig (useless)

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
   publicRuntimeConfig: {
     version,
   },
-  serverRuntimeConfig: {
-    APP_TITLE: "OKP4 Faucet devnet",
-    APP_KEYWORDS: "OKP4, Faucet, Blockchain, Know, Devnet, Token",
-    CHAIN_ID: "okp4-devnet-1",
-    CHAIN_NAME: "OKP4",
-    CHAIN_RPC_ENDPOINT: "https://api.devnet.staging.okp4.network:443/rpc",
-    CHAIN_REST_ENDPOINT: "https://api.devnet.staging.okp4.network",
-    FAUCET_URL: "https://faucet.devnet.staging.okp4.network/graphql",
-  },
   output: 'standalone',
 }
 
